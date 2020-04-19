75c284a991732f7ece54c48520f3f3134f19b3b8
AmitSamra <Amit.Samra@outlook.com>
2020-04-19 07:29:08 -0400

    Fixed print statements

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,12 @@
 a = [x for x in range(1,12,1)]
-item = 4
+item = 11
 
 def binary_search(list_in, item):
 
     if item in a:
 
         list_in = sorted(list_in)
-
+        print(list_in)
         half = len(list_in) // 2
 
         if list_in[half] == item:
@@ -15,18 +15,15 @@ def binary_search(list_in, item):
             return half
 
         elif len(list_in) == 1 or len(list_in) == 2:
-            print(list_in)
             print(f'Your number is {item}')
             return list_in.index(item)
 
         elif item < list_in[half]:
             list_a = list_in[0:half]
-            print(list_a)
             return binary_search(list_a, item)
 
         elif item > list_in[half]:
             list_a = list_in[half+1:]
-            print(list_a)
             return binary_search(list_a, item)
 
     else:
