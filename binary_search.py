9702eaeba5e2347e38262fcee45012bf3c6619da
AmitSamra <Amit.Samra@outlook.com>
2020-04-19 06:56:18 -0400

    Recursion works

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,24 @@
 a = [x for x in range(1,12,1)]
-item = 2
+item = 5
 
 def binary_search(list_in, item):
+
     list_in = sorted(list_in)
-    half = len(list_in)//2
+
+    half = len(list_in) // 2
+
     if list_in[half] == item:
         return half
-    elif len(list_in) == 2:
+
+    elif len(list_in) == 1 or len(list_in) == 2:
         return list_in.index(item)
-    else:
+
+    elif item < list_in[half]:
         list_a = list_in[0:half]
-        half_a = len(list_a)//2
-        if list_a[half_a] == item:
-            return half_a
-        else:
-            binary_search(list_a, item)
+        return binary_search(list_a, item)
+
+    elif item > list_in[half]:
+        list_a = list_in[half+1:]
+        return binary_search(list_a, item)
 
 print(binary_search(a,item))
