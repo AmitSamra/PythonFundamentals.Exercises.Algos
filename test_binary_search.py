fc2200eddefd95ba6ad54479593822e18c7fff24
AmitSamra <Amit.Samra@outlook.com>
2020-04-20 03:20:38 -0400

    Fixed edge case of last item in list

--- test_binary_search.py
+++ test_binary_search.py
@@ -4,14 +4,16 @@ import binary_search
 class TestBinary_Search(unittest.TestCase):
 
     def test_binary_search(self):
-        self.assertEqual(binary_search.binary_search([1,2,3],2),1)
-        self.assertEqual(binary_search.binary_search([1,2,3,4],3),2)
-
-
-
-
-
-
+        self.assertEqual(binary_search.binary_search([1, 2, 3], 1), 0)
+        self.assertEqual(binary_search.binary_search([1, 2, 3], 2), 1)
+        self.assertEqual(binary_search.binary_search([1, 2, 3], 3), 0)
+        self.assertEqual(binary_search.binary_search([1, 2, 3, 4], 1), 0)
+        self.assertEqual(binary_search.binary_search([1, 2, 3, 4], 2), 1)
+        self.assertEqual(binary_search.binary_search([1, 2, 3, 4], 3), 2)
+        self.assertEqual(binary_search.binary_search([1, 2, 3, 4], 4), 0)
+        self.assertEqual(binary_search.binary_search([1, 2], 1), 0)
+        self.assertEqual(binary_search.binary_search([1, 2], 2), 1)
+        self.assertEqual(binary_search.binary_search([1], 1), 0)
 
 if __name__ == '__main__':
 	unittest.main()
